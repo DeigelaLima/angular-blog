ddd361dc77e23873a83fb65789d8f6b5e59155a8
Deigela_Lima <deigelalima@hotmail.com>
2024-02-16 09:30:35 -0300

    content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover": "https://i.guim.co.uk/img/media/9b7412a06451584d8e594c9d08b3cd0623d23d02/0_32_1800_1081/master/1800.jpg?width=1200&quality=85&auto=format&fit=max&s=c852668beeb82679081ff608bef6734e"
+  },
+  {
+    "id": "2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Muito bom!",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2022/04/Revelado-qual-serie-da-Marvel-faz-mais-sucesso-na-Disney-legadodamarvel.jpg"
+  }
+]
